0ded7a902803328ef7b4ea2b0208c9dd517b2db2
LuisAlexisSalazar <luis.salazar.marroquin@ucsp.edu.pe>
2024-01-27 18:53:52 -0500

    Farmer and Timer

---
+++ Assets/Scripts/Timer/Timer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.IO;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timer = 60;
+
+    // public Text textoTimer;
+    public TextMeshProUGUI textoTimerPro;
+    private bool gameIsPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameIsPaused)
+        {
+            UpdateTimer();
+            if (timer <= 0)
+            {
+                Debug.Log("Termino el Juego: ");
+                PauseGame();
+            }
+        }
+    }
+
+    void UpdateTimer()
+    {
+        timer -= Time.deltaTime;
+        string formattedTimer = timer.ToString("00");
+        // textoTimer.text = "0:" + timer.ToString("f0");
+        textoTimerPro.text = "0:" + formattedTimer;
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0f; // Establecer el tiempo en 0 pausará el juego
+        gameIsPaused = true;
+    }
+
+    public float GetTimer()
+    {
+        return timer;
+    }
+}
